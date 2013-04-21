ff2bf20494e0070954efbd791475fbcab79a30b0
Michael Berkowitz <michael.berkowitz@gmail.com>
2013-04-21 00:57:21 -0400

    clean up specs

--- spec/webern_spec.rb
+++ spec/webern_spec.rb
@@ -3,10 +3,8 @@ require 'spec_helper'
 describe Webern do
   it 'should output correctly' do
     row = Webern::Row.new(11, 10, 2, 3, 7, 6, 8, 4, 5, 0, 1, 9)
-    puts
-    Webern::Formatters::Text.new(row).draw
-    Webern::Formatters::Text.new(row, false).write_to_file('/Users/mikowitz/Desktop/row.txt')
-    Webern::Formatters::Pdf.new(row).write_to_file('/Users/mikowitz/Desktop/row.pdf')
-    Webern::Formatters::Lilypond.new(row).write_to_file('/Users/mikowitz/Desktop/row.ly')
+    Webern::Formatters::Text.new(row, false).write_to_file('row.txt')
+    Webern::Formatters::Pdf.new(row).write_to_file('row.pdf')
+    Webern::Formatters::Lilypond.new(row).write_to_file('lilypond-row.ly')
   end
 end
