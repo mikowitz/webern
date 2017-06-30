386e86bb99477fb1723369d08b63a68c1f989214
Michael Berkowitz <michael.berkowitz@gmail.com>
2017-06-30 15:48:41 -0400

    Add accessors for getting transformations at specific steps

--- lib/webern/row.ex
+++ lib/webern/row.ex
@@ -33,6 +33,26 @@ defmodule Webern.Row do
     new(Enum.map(pitch_classes, &(normalize(&1 + interval))))
   end
 
+  def p(row = %__MODULE__{}, start) do
+    row |> zero |> transpose(start)
+  end
+
+  def r(row = %__MODULE__{}, start) do
+    row |> zero |> transpose(start) |> retrograde
+  end
+
+  def i(row = %__MODULE__{}, start) do
+    row |> zero |> inverse |> transpose(start)
+  end
+
+  def ri(row = %__MODULE__{}, start) do
+    row |> zero |> inverse |> transpose(start) |> retrograde
+  end
+
+  def ir(row = %__MODULE__{}, start) do
+    row |> zero |> transpose(start) |> retrograde |> inverse
+  end
+
   defp normalize(n) when n < 0, do: normalize(n + (-n * 12))
   defp normalize(n), do: rem(n, 12)
 end
