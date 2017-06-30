2a46ecfaa2305e37485cd3573d08866d180e32b7
Michael Berkowitz <michael.berkowitz@gmail.com>
2017-06-30 14:02:43 -0400

    mix new

---
+++ lib/webern.ex
@@ -0,0 +1,18 @@
+defmodule Webern do
+  @moduledoc """
+  Documentation for Webern.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Webern.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
